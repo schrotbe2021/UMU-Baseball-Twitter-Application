ce78f40265e8d3c4eb92c67091850b910eea54de
Ben Schroth <schrotbe2021@mountunion.edu>
2020-01-20 14:07:52 -0500

    Began building of UI for the app. Start with Twitter Verification Page then choose tweet to be filled later.

---
+++ app.py
@@ -0,0 +1,99 @@
+import Tkinter as tk     # python 2
+import tkFont as tkfont  # python 2
+
+class SampleApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (StartPage, ChooseTweet, StartGame, ScoringChange):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("StartPage")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Verify Twitter Account", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        
+        button3 = tk.Button(self, text="Choose Tweet",
+                            command=lambda: controller.show_frame("ChooseTweet"))
+    
+        #button1.pack()
+        #button2.pack()
+        button3.pack()
+
+
+class StartGame(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Start Game Page", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+
+class ScoringChange(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Scoring Change Game", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+class ChooseTweet(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Choose Tweet", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+
+        button1 = tk.Button(self, text="Start Game",
+                            command=lambda: controller.show_frame("StartGame"))
+        button2 = tk.Button(self, text="Scoring Chnage",
+                            command=lambda: controller.show_frame("ScoringChange"))
+        
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+        button1.pack()
+        button2.pack()
+
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()
