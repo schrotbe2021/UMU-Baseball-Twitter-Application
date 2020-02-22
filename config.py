6fb96d6580e9a2ed4477f20548961d268cc456e5
Ben Schroth <schrotbe2021@mountunion.edu>
2020-02-21 21:53:46 -0500

    Removed config

--- config.py
+++
@@ -1,4 +0,0 @@
-api_key = 'Kt4rgFjVqsEjUZ7hI2gogLgf3'
-api_secret = 'XnFrW7acLminsGdPTgiGxpsnSqDdhSu311LDcc0YsTsWTc1hHw'
-access_token = '804787884893028356-OSuVoDz3eduTcwq8B50qiXIec5OhFCg'
-token_secret = 'hTw8k48CIWkQvkbH8yYSrDx9tDav4GWCciPQV5xGOArxa'
